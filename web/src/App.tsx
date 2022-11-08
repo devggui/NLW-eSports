43d509bb112b86966a32de5bd7bd54915df09d8c
devggui <devguiet@gmail.com>
2022-11-08 20:08:56 -0300

    NLW Stage 2 - in progress

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/main.css';
+
+import logoImg from './assets/logo-esports.svg';
+
+function App() {
+  return (
+    <div>      
+      <img src={logoImg} />
+    </div>
+  )
+}
+
+export default App
